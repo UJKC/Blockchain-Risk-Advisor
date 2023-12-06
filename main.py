2ca632d434393ff4dfc0339074e9435d6be73a74
Ujwal KC <ujwalkcsps@gmail.com>
2023-12-06 08:52:52 +0530

    charts

--- main.py
+++ main.py
@@ -1,11 +1,36 @@
 from flask import Flask, render_template
+import requests
 
 app = Flask(__name__)
 
+def get_blockchain_chart_data():
+    # Make a request to the Blockchain Charts API
+    chart_name = 'transactions-per-second'
+    timespan = '5weeks'
+    rolling_average = '8hours'
+    format_type = 'json'
+
+    url = f'https://api.blockchain.info/charts/{chart_name}?timespan={timespan}&rollingAverage={rolling_average}&format={format_type}'
+    response = requests.get(url)
+
+    # Check if the request was successful (status code 200)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        # Handle errors as needed
+        return None
 
 @app.route('/')
 def home():
     return render_template('index.html')
+
+@app.route('/home')
+def index():
+    # Fetch Blockchain Charts data
+    chart_data = get_blockchain_chart_data()
+
+    # Render the HTML page and pass the chart data
+    return render_template('index.html', chart_data=chart_data)
 
 if __name__ == "__main__":
     app.run(debug=True)
